be9933dc90f3a316a65054a385ad0b1b41ea44a4
cdobson <dobco402@student.otago.ac.nz>
2020-08-16 21:10:06 +1200

    Added some Javascript to read the reviews.json file and display it on the webpage

--- showReviews.js
+++ showReviews.js
@@ -2,10 +2,35 @@ var showReviews = (function() {
     "use strict";
     var pub = {};
 
-    var reviews;
+    var customerReviews;
+    var index;
+
+    function displayReview() {
+        $("#reviews").empty();
+
+        $("#reviews").append("<h3>" + customerReviews[index].title + "</h3>");
+        $("#reviews").append("<p>" + customerReviews[index].author + "</p>");
+        $("#reviews").append("<p>" + customerReviews[index].reviewcontent + "</p>");
+        index++;
+
+        if (index === customerReviews.length) {
+            index = 0;
+        }
+    }
 
     pub.setup = function() {
-        // setup reading reviews from reviews.json
+        $.ajax ({
+            type: "GET",
+            url: "./Resources/reviews.json",
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                customerReviews = data;
+                index = 0;
+                displayReview();
+                setInterval(displayReview, 7000);
+            }
+        });
     }
 
     return pub;
