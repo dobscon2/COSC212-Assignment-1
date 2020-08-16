9751fdc7ffe3b1833f3e952521b6799c2bd64d31
cdobson <dobco402@student.otago.ac.nz>
2020-08-16 18:19:16 +1200

    Creating Javascript file to read and shuffle through reviews

---
+++ showReviews.js
@@ -0,0 +1,15 @@
+var showReviews = (function() {
+    "use strict";
+    var pub = {};
+
+    var reviews;
+
+    pub.setup = function() {
+        // setup reading reviews from reviews.json
+    }
+
+    return pub;
+
+}());
+
+$(document).ready(showReviews.setup);
