e64fc4186a5bea8ce0e0be5654ac3f3a32cd533e
cdobson <dobco402@student.otago.ac.nz>
2020-08-29 16:08:50 +1200

    Added code that puts size choices to users first then added onclick events

--- book.js
+++ book.js
@@ -5,6 +5,64 @@ var book = (function() {
 
     var pub = {};
 
+    function startBooking() {
+        $("#createBooking").append("<p>Please choose a car size.</p>");
+        $("#createBooking").append("<div class='size'>" +
+            "<img src='images/smallCar.jpg' alt='small car'>" +
+            "<figcaption>Small Car</figcaption></div>" +
+            "<div class='size'>" +
+            "<img src='images/mediumCar.jpg' alt='medium car'>" +
+            "<figcaption>Medium Car</figcaption></div>" +
+            "<div class='size'>" +
+            "<img src='images/largeCar.jpg' alt='large car'>" +
+            "<figcaption>Large Car</figcaption></div>" +
+            "<div class='size'>" +
+            "<img src='images/luxuryCar.jpg' alt='luxury car'>" +
+            "<figcaption>Luxury Car</figcaption></div>");
+
+        $(".size").find("img").each(function() {
+            var imageCall = $(this).attr("src");
+
+            imageCall = imageCall.replace("images/", "");
+            imageCall = imageCall.replace(".jpg", "");
+
+            switch (imageCall) {
+                case "smallCar":
+                    $(this).click(smallCar);
+                    $(this).css('cursor', 'pointer');
+                    break;
+                case "mediumCar":
+                    $(this).click(mediumCar);
+                    $(this).css('cursor', 'pointer');
+                    break;
+                case "largeCar":
+                    $(this).click(largeCar);
+                    $(this).css('cursor', 'pointer');
+                    break;
+                case "luxuryCar":
+                    $(this).click(luxuryCar);
+                    $(this).css('cursor', 'pointer');
+                    break;
+            }
+        })
+    }
+
+    function smallCar() {
+        console.log("Small cars");
+    }
+
+    function mediumCar() {
+        console.log("Medium Cars");
+    }
+
+    function largeCar() {
+        console.log("Large Cars");
+    }
+
+    function luxuryCar() {
+        console.log("Luxury Cars");
+    }
+
     pub.setup = function() {
         $.ajax ({
             type: "GET",
@@ -13,7 +71,7 @@ var book = (function() {
             cache: false,
             success: function(data) {
                 booked = data;
-                console.log(booked);
+                startBooking();
             },
 
             error: function() {
